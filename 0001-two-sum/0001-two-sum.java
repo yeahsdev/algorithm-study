4b22de2973a773e2d58fe04b323889e41633ed11
yeahsdev <98158230+yeahsdev@users.noreply.github.com>
2025-02-04 15:39:12 +0900

    Time: 4 ms (60.81%), Space: 45.1 MB (44.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] numbers = new int[2];
-        for(int i =0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target){
-                    numbers[0] = i;
-                    numbers[1] = j;
-                }
-            }
-            
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int length = nums.length;
+
+        for(int i=0;i<length;i++) {
+            numMap.put(nums[i],i);
         }
-        return numbers;
+
+        // 키값에 해당하는 반대값 찾기
+        for(int i=0;i<length;i++){
+            int complement = target - nums[i];
+            if(numMap.containsKey(complement) && numMap.get(complement) != i){
+                return new int[]{i,numMap.get(complement)};
+            }
+        }
+
+        return new int[]{}; // no solution found
     }
 }
