eb5472b50ab113aed2a7b67189a249749215735f
yeahsdev <98158230+yeahsdev@users.noreply.github.com>
2025-02-07 12:15:01 +0900

    Time: 6 ms (24.04%), Space: 42.5 MB (25.22%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int word1Len = word1.length();
+        int word2Len = word2.length();
+        String newStr = "";
+
+        for(int i=0;i<word1Len;i++) {
+            newStr = newStr + word1.charAt(i);
+            // word1 순서 마지막이면 끝내기
+            if(i == word1Len-1) {
+                if(i<word2Len) {
+                    newStr = newStr + word2.substring(i);
+                    return newStr;
+                }
+                
+            }
+            if(i<word2Len) {
+                newStr = newStr + word2.charAt(i);
+            }
+            
+        }
+
+        return newStr;
+    }
+}
