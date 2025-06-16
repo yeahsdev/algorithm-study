47962cd16469dc79d249288642470ad94c65c071
Yeseol Im <98158230+yeahsdev@users.noreply.github.com>
2025-06-16 11:09:05 +0900

    Update 0102-binary-tree-level-order-traversal.java

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,8 +15,6 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root); // 큐에 루트 노드 추가
 
         List<List<Integer>> results = new ArrayList<>();
         // 엣지 케이스: 루트가 null이면 빈 리스트 반환
@@ -24,9 +22,13 @@ class Solution {
             return results;
         }
         /**
-        현재 레벨에서 left right 노드를 list 에 넣기
+        현재 레벨에 저장할 리스트 생성 및 add
+        현재 레벨 노드 순회 끝나면 현재레벨에 저장된 리스트를 결과 리스트에 추가
          */
          // BFS를 위한 큐(Queue) 선언
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root); // 큐에 루트 노드 추가
         
         while(!q.isEmpty()){
             int levelSize = q.size();
